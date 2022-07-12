80534cdc80ea9be3b702aedd5052583e9b8b0daf
eloisasilva2 <108958793+eloisasilva2@users.noreply.github.com>
2022-07-12 20:33:39 -0300

    Delete Exercicio_02 directory

--- Exercicio_02/exercicio_02.js
+++
@@ -1,6 +0,0 @@
-var nome = window.prompt("Digite seu nome aqui:");
-var email = window.prompt("Digite seu e-mail aqui:");
-
-document.write("<h4> Nome: " + nome + "<br> E-mail: " + email + "</h4>");
-
-
