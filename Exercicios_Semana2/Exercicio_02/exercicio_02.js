01a7b98ff5c189669b14a5b0e7ca613889a06f0d
eloisasilva2 <eloisa_silva2@estudante.sesisenai.org.br>
2022-07-12 21:38:06 -0300

    Adicionando os exercicios da semana 2

---
+++ Exercicios_Semana2/Exercicio_02/exercicio_02.js
@@ -0,0 +1,6 @@
+var nome = window.prompt("Digite seu nome aqui:");
+var email = window.prompt("Digite seu e-mail aqui:");
+
+document.write("<h4> Nome: " + nome + "<br> E-mail: " + email + "</h4>");
+
+
