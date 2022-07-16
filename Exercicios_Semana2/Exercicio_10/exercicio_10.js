6eccab489b13bfbc180a68dfb73fa26a6ec265c2
eloisasilva2 <eloisa_silva2@estudante.sesisenai.org.br>
2022-07-16 16:07:16 -0300

    Adicionando exercíco 10 da semana 2

--- Exercicios_Semana2/Exercicio_10/exercicio_10.js
+++ Exercicios_Semana2/Exercicio_10/exercicio_10.js
@@ -1,26 +1,30 @@
-var contador = 0,
-  resposta,
-  qtdPessoas = 0;
+//Pensando em um sistema de notas online, crie um código que irá capturar o nome do aluno e a nota de 4 unidades usando o prompt dentro de uma laço while, a ideia é que a cada valor digitado seja puxado para um array com o método push. Após isso, crie um laço “for” que irá calcular a soma de todos os itens do array para ser calculada sua média. Ao final de tudo, deverá ser apresentado em tela o nome do aluno, sua média e se o aluno foi aprovado ou não, para esse último tópico considere a média para aprovação maior ou igual a 7.
 
-while (contador < 4) {
-  resposta = window.prompt("Qual a sua avaliação para a série Stranger things? Responda com as opções: ruim, bom ou excelente."); 
-  switch (resposta) { 
-    case "ruim":
-      qtdPessoas++;
-      break;
-    case "bom":
-      break;
-    case "excelente":
-      break;
-    default:
-      resposta = window.prompt(
-        "É permitido apenas digitar uma dessas opções: ruim, bom ou excelente.\n" + "Digite novamente a sua avaliação."
-      ); 
-      if (resposta == "ruim") { 
-        qtdPessoas++; 
-      }
-  }
-  contador++;
+
+
+var nota = [],
+  aluno,
+  unidades = 0;
+
+aluno = window.prompt("Qual o nome do aluno?");
+while (unidades < 4) {
+  nota.push(parseFloat(window.prompt("Qual a nota?")));
+  unidades++;
 }
 
-document.write("<h3> Quantidade de pessoas que avaliaram a série como ruim: " + qtdPessoas + "</h3>");
+var soma = 0;
+
+for (var i = 0; i < nota.length; i++) {
+  soma += nota[i];
+}
+
+let media = soma / nota.length;
+
+if (media >= 7) {
+  resultado = "Aprovado";
+} else {
+  resultado = "Reprovado"
+}
+
+
+document.write('<h3> Nome do Aluno: ' + aluno + '</br> Média: ' + media + '</br> Resultado: ' + resultado + '</h3>');
