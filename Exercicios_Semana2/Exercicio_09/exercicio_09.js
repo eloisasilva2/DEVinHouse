9254170f9dc6cb8b5d3cc696b9f996e4550d9a0d
eloisasilva2 <eloisa_silva2@estudante.sesisenai.org.br>
2022-07-16 14:51:04 -0300

    Adicionando exercíco 9 da semana 2

--- Exercicios_Semana2/Exercicio_09/exercicio_09.js
+++ Exercicios_Semana2/Exercicio_09/exercicio_09.js
@@ -1,8 +1,18 @@
+//Você foi contratado por um supermercado para catalogar a compra de uma cliente. Assim que a aplicação começa deverá ser perguntado em um prompt qual produto o cliente deseja comprar, só serão aceitas as seguintes respostas:
+// (1) Hortifruti
+// (2) Laticínios
+// (3) Carnes
+// (4) Peixes
+// (5) Aves
+// (6) Fechar pedido
+// Após fazer a seleção do produto pergunte a quantidade de itens que o cliente irá comprar.
+// Apenas se o cliente selecionar a opção 6 é que a pergunta deverá parar de ser repetida.
+// Ao selecionar a opção 6 imprima o produto que o cliente pegou em maior quantidade.
+
 
 var resposta;
-var produto = [''];
+var prodMaiorQuant = '';
 var qtd = [0, 0, 0, 0, 0];
-var fim = false;
 
 do {
   resposta = window.prompt(
@@ -12,28 +22,36 @@ do {
     window.alert("É possível digitar apenas o código de uma das opções: (1) Hortifruti / (2) Laticínios / (3) Carnes / (4) Peixes / (5) Aves / (6) Fechar pedido");
   } else if (resposta == 1) {
     qtd[0] = parseFloat(window.prompt("Qual a quantidade de Hortifruti deseja?"));
-      produto.push("Hortifruti");
-  }  else if (resposta == 2){
+
+  } else if (resposta == 2) {
     qtd[1] = parseFloat(window.prompt("Qual a quantidade de Laticínios deseja?"));
-    produto.push("Laticínios");
-  }  else if (resposta == 3){
+
+  } else if (resposta == 3) {
     qtd[2] = parseFloat(window.prompt("Qual a quantidade de Carnes deseja?"));
-    produto.push("Carnes");
-  } else if (resposta == 4){
+
+  } else if (resposta == 4) {
     qtd[3] = parseFloat(window.prompt("Qual a quantidade de Peixes deseja?"));
-    produto.push("Peixes");
-  } else if (resposta == 5){
+
+  } else if (resposta == 5) {
     qtd[4] = parseFloat(window.prompt("Qual a quantidade de Aves deseja?"));
-    produto.push("Aves");
-  }
 
   }
 
-
+}
 while (resposta != 6) {
-
-
-      
-  }
-
-document.write("<h3> O produto que o cliente mais pediu foi " + produto + "com a seguinte quantidade: " + qtd + "</h3>");
+  var maior = Math.max.apply(null, qtd);
+  if (maior == 0){
+    prodMaiorQuant = "Nenhum produto foi selecionado.";
+  } else if (maior == qtd[0]) {
+    prodMaiorQuant = "Hortifruti";
+  } else if (maior == qtd[1]) {
+    prodMaiorQuant = "Laticínios";
+  } else if (maior == qtd[2]) {
+    prodMaiorQuant = "Carnes";
+  } else if (maior == qtd[3]) {
+    prodMaiorQuant = "Peixes";
+  } else if (maior == qtd[4]) {
+    prodMaiorQuant = "Aves";
+}
+}
+document.write("<h3> Produto com maior quantidade: " + prodMaiorQuant + "<br> Total: " + maior + "</h3>");
