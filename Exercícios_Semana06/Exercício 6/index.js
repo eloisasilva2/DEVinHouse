97b8882d7d7e5f6c555be8217fc05cd0b70a6c04
eloisasilva2 <eloisa_silva2@estudante.sesisenai.org.br>
2022-08-06 21:44:45 -0300

    Adicionando exercício 06 da Semana 06

--- Exercícios_Semana06/Exercício 6/index.js
+++ Exercícios_Semana06/Exercício 6/index.js
@@ -5,3 +5,40 @@ No input com a classe input__search será onde o usuário poderá pesquisar pelo
 No span com o id pokemontype será adicionado o tipo do pokémon pesquisado.
 No span com o id weight irá ser apresentado o peso do pokemon.
 Ao clicar no botão com a classe button será iniciado toda a funcionalidade da aplicação: pesquisa da api, adição das informações nos campos… */
+
+
+
+
+var botao = document.querySelector('.button');
+botao.addEventListener('click', (Pesquisar));
+
+function Pesquisar() {
+
+    let pokemon = document.querySelector(".input__search").value;
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.onload = () => {
+
+        pokemonnumber.innerHTML = xhr.response.id;
+        pokemonname.innerHTML = xhr.response.name;
+        pokemontype.innerHTML = xhr.response.types[0].type.name;
+        weight.innerHTML = xhr.response.weight;
+
+
+        let imagem = xhr.response.sprites.front_default
+        document.querySelector(".pokemonimage").setAttribute("src", imagem);
+
+        ability.innerHTML = "";
+        xhr.response.abilities.forEach((item) => {
+            let tagLi = document.createElement("li");
+            tagLi.innerHTML += item.ability.name;
+            ability.appendChild(tagLi);
+        });
+    }
+
+    xhr.onerror = (err) => console.log(err);
+    xhr.open("GET", `https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    xhr.responseType = "json";
+    xhr.send();
+}
