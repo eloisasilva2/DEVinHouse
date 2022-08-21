252d3893fd19e30920d4c453f75df422f3c7c414
eloisasilva2 <eloisa_silva2@estudante.sesisenai.org.br>
2022-08-20 22:28:38 -0300

    Iniciando semana 08

---
+++ Desktop/Curso Senai/Exercicios/Semana08/Exercícios_Semana08/Exercício_05/exercicio-05/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Soma from './components/Soma.js';
+
+function App() {
+    return (
+        <div>
+            <p>
+                <Soma numero1={5} numero2={7} />
+            </p>
+        </div>
+
+    );
+
+
+}
+
+export default App;
---
+++ Desktop/Curso Senai/Exercicios/Semana08/Exercícios_Semana08/meu-primeiro-projeto-react/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Titulo from './components/Titulo';
+import Texto from './components/Texto';
+
+function App() {
+  return (
+    <div>
+    <h1>
+    <Titulo/>  
+    </h1>
+    <i>
+    <Texto/> 
+    </i>
+    </div>
+    );
+}
+
+
+export default App;
+
